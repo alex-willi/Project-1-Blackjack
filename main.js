694713a92f30d9a6548757710e53627bac5843ec
alex-willi <alex.lawson.williams@gmail.com>
2022-11-15 19:11:39 -0600

    first part of function to calculate total hand value.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const deal = document.querySelector("#deal")
-const hit = document.querySelector("#hit")
+// const deal = document.querySelector("#deal")
+// const hit = document.querySelector("#hit")
+// const pCards = document.querySelector("#player-cards")
 
 let deckOfCards = ['as', 'ah', 'ac','ad','ks','kh','kc','kd','qs','qh','qc','qd','js','jh','jd','js','10s','10h','10c','10d','9s','9h','9c','9d','8s','8h','8c','8d','7s','7h','7c','7d','6s','6h','6c','6d','5s','5h','5c','5d','4s','4h','4c','4d','3s','3h','3c','3d','2s','2h','2c','2d',]
 
@@ -7,31 +8,44 @@ let playerHand = []
 
 let dealerHand = []
 
-function dealP(){
-    playerHand.push(deckOfCards.splice(Math.floor(Math.random()* deckOfCards.length), 1))
+let calcTotal = function (first) {
+    for (let i = 0; i < first.length; i += 1) {
+         let x = first[i].charAt(0)
+         if (x == 'k' || x == '1'|| x == 'q' || x == 'j'){
+         x = '10'
+         }
+         if (x == 'a'){
+            x = 11
+         }
+        let h = parseInt(x)
+        console.log(h)
+    }
 }
-
-function dealD(){
-    dealerHand.push(deckOfCards.splice(Math.floor(Math.random()* deckOfCards.length), 1))
-}
+    calcTotal(deckOfCards)
 
 
+// function dealP(){
+//     playerHand.push(deckOfCards.splice(Math.floor(Math.random()* deckOfCards.length), 1))
+// }
+
+// function dealD(){
+//     dealerHand.push(deckOfCards.splice(Math.floor(Math.random()* deckOfCards.length), 1))
+// }
 
 
-deal.addEventListener('click',() => {
-    dealD()
-    dealD()
-    dealP()
-    dealP()
-    console.log(playerHand)
 
-})
 
-hit.addEventListener('click', () => {
-    if(playerHand.length >= 2)
-    dealP()
-    console.log(playerHand)
+// deal.addEventListener('click',() => {
+//     dealD()
+//     dealD()
+//     dealP()
+//     dealP()
+//     pCards.textContent = playerHand.toString()
+// })
 
-})
-console.log(playerHand)
+// hit.addEventListener('click', () => {
+//     if(playerHand.length >= 2)
+//     dealP()
+//     pCards.textContent = playerHand.toString()
+// })
 
